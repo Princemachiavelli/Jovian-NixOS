481db98ed7367953c69685641c0747894ae2f9af
Josh Hoffer <jhoffer@sansorgan.es>
2022-08-12 21:31:44 -0600

    add x86_64-linux package outputs

--- flake.nix
+++ flake.nix
@@ -2,18 +2,31 @@
   description = "Jupiter/SteamDeck compatible NixOS";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
-    lib-aggregate.url = "github:nix-community/lib-aggregate";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    #lib-aggregate.url = "github:nix-community/lib-aggregate";
   };
 
-  outputs = {self, nixpkgs, lib-aggregate}@inputs:
+  outputs = {self, nixpkgs, flake-utils, ... }@inputs:
   {
-
     nixosModules.jovian = import ./modules;
     nixosModule = self.nixosModules.jovian;
 
     overlays.jovian = import ./overlay.nix;
     overlay = self.overlays.jovian;
-  }; 
-
+  } // (with flake-utils.lib; eachSystem [ "x86_64-linux" ] (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ self.overlay ];
+      };
+    in rec {
+      packages = {
+        gamescope = pkgs.gamescope;
+        acp5x-ucm = pkgs.acp5x-ucm;
+        jupiter-fan-control = pkgs.jupiter-fan-control;
+        linux-firmware = pkgs.linux-firmware;
+        linux_jovian = pkgs.linux_jovian;
+      };
+    }));
 }
