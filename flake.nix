b8c92b38ad6d76019249308d3ea85308d16e6d0a
Josh Hoffer <jhoffer@sansorgan.es>
2022-08-12 18:30:25 -0600

    trim to just module and overlay

--- flake.nix
+++ flake.nix
@@ -3,15 +3,17 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
+    lib-aggregate.url = "github:nix-community/lib-aggregate";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = {self, nixpkgs, lib-aggregate}@inputs:
   {
 
     nixosModules.jovian = import ./modules;
     nixosModule = self.nixosModules.jovian;
 
-    overlay = import ./overlay.nix;
-  };
+    overlays.jovian = import ./overlay.nix;
+    overlays.default = self.overlays.jovian;
+  }; 
 
 }
